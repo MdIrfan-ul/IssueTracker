cdf37570d469f7c436dcf5c82ca92a5d5e4aa3b9
Mohamed Irfanullah M <mohamedirfan1720@gmail.com>
2024-04-21 02:35:45 +0530

    issue Model and Schema added

---
+++ src/models/issue.model.js
@@ -0,0 +1,13 @@
+import { IssueModel } from "./issue.Schema.js";
+
+export const createIssue = async (title,description,labels,author,projectId)=>{
+    return await   new IssueModel({title,description, labels,author,projectId}).save();
+}
+
+export const getIssues = async ()=>{
+    return await IssueModel.find({});
+}
+export const getIssuesById = async (projectId)=>{
+    return await IssueModel.find({projectId:projectId});
+}
+
