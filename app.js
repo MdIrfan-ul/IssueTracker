930c4986565a885e3463415c3dc9c8bdfb40823b
Mohamed Irfanullah M <mohamedirfan1720@gmail.com>
2024-06-20 13:43:38 +0530

    User Routes Added

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ import EjsLayouts from "express-ejs-layouts";
 import path from "path";
 import ProjectController from "./src/controllers/project.controller.js";
 import IssueController from "./src/controllers/issue.controller.js";
+import UserController from "./src/controllers/user.controller.js";
 import { connectToDB } from "./config/mongoose.db.js";
+import session from "express-session";
+import cookieParser from "cookie-parser";
+import { auth } from "./src/middlewares/auth.middleware.js";
+import { setLastVisit } from "./src/middlewares/lastVisit.middleware.js";
+// import userController from "./src/controllers/user.controller.js";
 
 connectToDB();
 const server = express();
@@ -15,25 +21,41 @@ server.set("views",path.join(path.resolve(),"src","views"));
 server.use(EjsLayouts);
 server.use(express.urlencoded({extended:true}));
 
+server.use(session({
+    secret:'M7xOu3ux1S',
+    resave:false,
+    saveUninitialized:true,
+    cookie:{secure:false},
+}));
+server.use(cookieParser());
 
-
+const userController = new UserController();
 const projectController = new ProjectController();
 const issueController = new IssueController();
 
+// user Routes
+server.get('/register',userController.getRegister);
+server.get('/login',userController.getLoginPage);
+server.post('/register',userController.addRegister);
+server.post('/login',userController.LoginUser);
+server.get("/logout",userController.logOut);
+
+
 // project Routes
-server.get("/",projectController.getProjects);
-server.get("/create",projectController.getNewProjects);
-server.post("/create",projectController.newProjects);
-server.get("/projectDetails/:id",projectController.projectDetails);
-server.get("/delete/:id",projectController.deleteProjects);
+server.get("/",setLastVisit,auth,projectController.getProjectsPage);
+server.get("/create",auth,projectController.getNewProjects);
+server.post("/create",auth,projectController.newProjects);
+server.get("/projectDetails/:id",auth,projectController.projectDetails);
+server.post("/delete/:id",auth,projectController.deleteProjects);
 
 // issue Routes
 server.post("/filter/:id",issueController.filteredIssue);
-server.get("/newIssue/:id",issueController.getIssuePage);
-server.post("/newIssue/:id",issueController.newIssue);
-server.get("/deleteIssue/:id",issueController.deleteIssue);
+server.get("/newIssue/:id",auth,issueController.getIssuePage);
+server.post("/newIssue/:id",auth,issueController.newIssue);
+server.get("/deleteIssue/:id",auth,issueController.deleteIssue);
 server.get("/404",projectController.getErrorpage);
 server.use(express.static("src/views"));
+
 
 // const port = 3200;
 server.listen(process.env.PORT);
